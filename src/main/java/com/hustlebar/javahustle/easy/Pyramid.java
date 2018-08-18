f691a27b2c6df539ec4e01e0aeb8aeaafe436920
Thamizharasu S <zarub2k@gmail.com>
2018-08-18 13:14:31 +0530

    Work started

---
+++ src/main/java/com/hustlebar/javahustle/easy/Pyramid.java
@@ -0,0 +1,11 @@
+package com.hustlebar.javahustle.easy;
+
+public class Pyramid {
+    public void construct() {
+        int row = 3;
+
+        for (int i = 0; i < row; i++) {
+            
+        }
+    }
+}
